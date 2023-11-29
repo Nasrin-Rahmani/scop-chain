d6c8b01c54db5c7500ec4e701ac2ed61f784284d
Nasrin-Rahmani <120251419+Nasrin-Rahmani@users.noreply.github.com>
2023-11-29 22:37:50 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+'use strict';
+/*
+function calculateAge(birthYear) {
+  const age = 2023 - birthYear;
+  return age;
+}
+
+const firstName = 'Morsal';
+calculateAge(1935);
+
+let abc = `a
+b
+c
+d`;
+console.log(abc);
+
+let hour = 11;
+let greeting;
+if (hour > 10) {
+  greeting = `good day`;
+}
+console.log(greeting);
+
+const number = 8;
+if (number % 2 == 0) {
+  console.log(`the number is even`);
+} else {
+  console.log(`the number is odd`);
+}
+
+let a = '1' + 1;
+console.log(a);
+let b = '5' - null;
+console.log(b);
+let c = '5';
+console.log(typeof c);
+let d = +c;
+console.log(typeof d);
+console.log(d);
+let e = 'John';
+let f = +e;
+console.log(typeof f);
+console.log(typeof e);
+
+let money;
+if (money) {
+  console.log(`there is enough money in your pucket! `);
+} else {
+  console.log(`sorry! you run out of money!`);
+}
+
+let t = +3;
+let u = -3;
+if (t === u) {
+  console.log('true bro');
+} else {
+  console.log('false bro');
+}
+
+const day = 'saturday';
+switch (day) {
+  case 'friday!':
+    console.log('Happy friday!');
+    break;
+  case 'saturday':
+    console.log('its saturday!');
+    break;
+  case 'sunday':
+    console.log('its sundat!');
+    break;
+  case 'monday':
+    console.log('its monday!');
+    break;
+  case 'tuesday':
+    console.log('its tuesday!');
+    break;
+  default:
+    console.log('its thursday!');
+}
+
+// prompt
+let morsal = prompt('hi dear, write your name first.');
+console.log(morsal);
+
+let fav_number = Number(prompt('what is your fav number dear?'));
+console.log(fav_number, typeof fav_number);
+
+let q = 'Hello world';
+console.log(q.length);
+let s = q.slice(6, 11);
+console.log(s);
+
+let aa = 6;
+let bb = aa--;
+console.log(`a= ${aa}, b= ${bb}`);
+
+let age = 18;
+let eligible = age >= 18 ? 'you can vote.' : "you can't vote.";
+console.log(eligible);
+
+let name = prompt('what is your name?');
+if (name != null) {
+  console.log(`hello  ${name} ! how can I help you`);
+}
+*/
+let length = 'hello';
+console.log(length.length);
+console.log(length.slice(2, 5));
